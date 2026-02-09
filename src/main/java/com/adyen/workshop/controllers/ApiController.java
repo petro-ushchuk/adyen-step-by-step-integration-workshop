0ab908b2c67d2d138d48e220d1bcb535ed49a833
Petro Ushchuk <petro.ushchuk+axsps@axcessps.com>
2026-02-09 13:22:17 +0100

    Implement Adyen payment integration with Drop-in support, including payment methods, 3DS2 authentication, and webhook handling

--- src/main/java/com/adyen/workshop/controllers/ApiController.java
+++ src/main/java/com/adyen/workshop/controllers/ApiController.java
@@ -43,29 +43,119 @@ public class ApiController {
     // Step 7
     @PostMapping("/api/paymentMethods")
     public ResponseEntity<PaymentMethodsResponse> paymentMethods() throws IOException, ApiException {
+        var paymentMethodsRequest = new PaymentMethodsRequest();
+        paymentMethodsRequest.setMerchantAccount(applicationConfiguration.getAdyenMerchantAccount());
 
-        return ResponseEntity.ok().body(null);
+        log.info("Retrieving available Payment Methods from Adyen {}", paymentMethodsRequest);
+        var response = paymentsApi.paymentMethods(paymentMethodsRequest);
+        log.info("Payment Methods response from Adyen {}", response);
+        return ResponseEntity.ok().body(response);
     }
 
     // Step 9 - Implement the /payments call to Adyen.
     @PostMapping("/api/payments")
     public ResponseEntity<PaymentResponse> payments(@RequestBody PaymentRequest body) throws IOException, ApiException {
+        var paymentRequest = new PaymentRequest();
 
-        return ResponseEntity.ok().body(null);
+        var amount = new Amount()
+                .currency("EUR")
+                .value(9998L);
+        paymentRequest.setAmount(amount);
+        paymentRequest.setMerchantAccount(applicationConfiguration.getAdyenMerchantAccount());
+        paymentRequest.setChannel(PaymentRequest.ChannelEnum.WEB);
+
+        paymentRequest.setPaymentMethod(body.getPaymentMethod());
+
+        var orderRef = UUID.randomUUID().toString();
+        paymentRequest.setReference(orderRef);
+        // The returnUrl field basically means: Once done with the payment, where should the application redirect you?
+        paymentRequest.setReturnUrl("http://localhost:8080/handleShopperRedirect");
+
+
+        // Step 12 3DS2 Redirect - Add the following additional parameters to your existing payment request for 3DS2 Redirect:
+        // Note: Visa requires additional properties to be sent in the request, see documentation for Redirect 3DS2: https://docs.adyen.com/online-payments/3d-secure/redirect-3ds2/web-drop-in/#make-a-payment
+        var authenticationData = new AuthenticationData();
+        authenticationData.setAttemptAuthentication(AuthenticationData.AttemptAuthenticationEnum.ALWAYS);
+        paymentRequest.setAuthenticationData(authenticationData);
+
+        // Change the following lines, if you want to enable the Native 3DS2 flow:
+        // Note: Visa requires additional properties to be sent in the request, see documentation for Native 3DS2: https://docs.adyen.com/online-payments/3d-secure/native-3ds2/web-drop-in/#make-a-payment
+        //authenticationData.setThreeDSRequestData(new ThreeDSRequestData().nativeThreeDS(ThreeDSRequestData.NativeThreeDSEnum.PREFERRED));
+        //paymentRequest.setAuthenticationData(authenticationData);
+
+        paymentRequest.setOrigin("https://localhost:8080");
+        paymentRequest.setBrowserInfo(body.getBrowserInfo());
+        paymentRequest.setShopperIP("192.168.0.1");
+        paymentRequest.setShopperInteraction(PaymentRequest.ShopperInteractionEnum.ECOMMERCE);
+
+        var billingAddress = new BillingAddress();
+        billingAddress.setCity("Amsterdam");
+        billingAddress.setCountry("NL");
+        billingAddress.setPostalCode("1012KK");
+        billingAddress.setStreet("Rokin");
+        billingAddress.setHouseNumberOrName("49");
+        paymentRequest.setBillingAddress(billingAddress);
+
+        // Step 11 - Optionally add the idempotency key
+        var requestOptions = new RequestOptions();
+        requestOptions.setIdempotencyKey(UUID.randomUUID().toString());
+
+        log.info("PaymentsRequest {}", paymentRequest);
+        var response = paymentsApi.payments(paymentRequest, requestOptions); // add RequestOptions here
+        log.info("PaymentsResponse {}", response);
+
+        return ResponseEntity.ok().body(response);
     }
 
-    // Step 13 - Handle details call (triggered after Native 3DS2 flow)
+    // Step 13 - Handle details call (triggered after the Native 3DS2 flow, called from the frontend in step 14)
     @PostMapping("/api/payments/details")
     public ResponseEntity<PaymentDetailsResponse> paymentsDetails(@RequestBody PaymentDetailsRequest detailsRequest) throws IOException, ApiException
     {
-
-        return ResponseEntity.ok().body(null);
+        log.info("PaymentDetailsRequest {}", detailsRequest);
+        var response = paymentsApi.paymentsDetails(detailsRequest);
+        log.info("PaymentDetailsResponse {}", response);
+        return ResponseEntity.ok().body(response);
     }
 
     // Step 14 - Handle Redirect 3DS2 during payment.
     @GetMapping("/handleShopperRedirect")
     public RedirectView redirect(@RequestParam(required = false) String payload, @RequestParam(required = false) String redirectResult) throws IOException, ApiException {
+        var paymentDetailsRequest = new PaymentDetailsRequest();
 
-        return null;
+        PaymentCompletionDetails paymentCompletionDetails = new PaymentCompletionDetails();
+
+        // Handle redirect result or payload
+        if (redirectResult != null && !redirectResult.isEmpty()) {
+            // For redirect, you are redirected to an Adyen domain to complete the 3DS2 challenge
+            // After completing the 3DS2 challenge, you get the redirect result from Adyen in the returnUrl
+            // We then pass on the redirectResult
+            paymentCompletionDetails.redirectResult(redirectResult);
+        } else if (payload != null && !payload.isEmpty()) {
+            paymentCompletionDetails.payload(payload);
+        }
+
+        paymentDetailsRequest.setDetails(paymentCompletionDetails);
+
+        var paymentsDetailsResponse = paymentsApi.paymentsDetails(paymentDetailsRequest);
+        log.info("PaymentsDetailsResponse {}", paymentsDetailsResponse);
+
+        // Handle response and redirect user accordingly
+        var redirectURL = "http://localhost:8080/result/"; // Update your url here by replacing `http://localhost:8080` with where your application is hosted (if needed)
+        switch (paymentsDetailsResponse.getResultCode()) {
+            case AUTHORISED:
+                redirectURL += "success";
+                break;
+            case PENDING:
+            case RECEIVED:
+                redirectURL += "pending";
+                break;
+            case REFUSED:
+                redirectURL += "failed";
+                break;
+            default:
+                redirectURL += "error";
+                break;
+        }
+        return new RedirectView(redirectURL + "?reason=" + paymentsDetailsResponse.getResultCode());
     }
 }
--- src/main/java/com/adyen/workshop/controllers/WebhookController.java
+++ src/main/java/com/adyen/workshop/controllers/WebhookController.java
@@ -4,18 +4,14 @@ import com.adyen.model.notification.NotificationRequest;
 import com.adyen.model.notification.NotificationRequestItem;
 import com.adyen.util.HMACValidator;
 import com.adyen.workshop.configurations.ApplicationConfiguration;
-import org.apache.coyote.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.security.SignatureException;
 
 /**
@@ -38,6 +34,29 @@ public class WebhookController {
     // Step 16 - Validate the HMAC signature using the ADYEN_HMAC_KEY
     @PostMapping("/webhooks")
     public ResponseEntity<String> webhooks(@RequestBody String json) throws Exception {
-        return ResponseEntity.accepted().build();
+        log.info("Received: {}", json);
+        var notificationRequest = NotificationRequest.fromJson(json);
+        var notificationRequestItem = notificationRequest.getNotificationItems().stream().findFirst();
+
+        try {
+            NotificationRequestItem item = notificationRequestItem.get();
+
+            // Step 16 - Validate the HMAC signature using the ADYEN_HMAC_KEY
+            if (!hmacValidator.validateHMAC(item, this.applicationConfiguration.getAdyenHmacKey())) {
+                log.warn("Could not validate HMAC signature for incoming webhook message: {}", item);
+                return ResponseEntity.unprocessableEntity().build();
+            }
+
+            // Success, log it for now
+            log.info("Received webhook with event {}", item.toString());
+
+            return ResponseEntity.accepted().build();
+        } catch (SignatureException e) {
+            // Handle invalid signature
+            return ResponseEntity.unprocessableEntity().build();
+        } catch (Exception e) {
+            // Handle all other errors
+            return ResponseEntity.status(500).build();
+        }
     }
 }
